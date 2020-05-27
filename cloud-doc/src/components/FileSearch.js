753098b7a738e5844f26a357a06b49b1d08e7b6a
2268849815 <2268849815@qq.com>
2020-05-27 22:42:09 +0800

    react-router源码

---
+++ cloud-doc/src/components/FileSearch.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+
+const FileSearch = ({ title, onFileSearch }) => {
+  const [ inputActive, setInputActive ] = useState(false)
+  const [ value, setValue ] = useState(0)
+}
