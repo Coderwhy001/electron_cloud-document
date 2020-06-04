ee601fb953288c46fb8e7249a37b8e374e12576a
2268849815 <2268849815@qq.com>
2020-06-04 21:03:21 +0800

    设置窗口开发

--- cloud-doc/main.js
+++ cloud-doc/main.js
@@ -1,18 +1,32 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const isDev = require('electron-is-dev')
+const AppWindow = require('./src/AppWindow')
 const menuTemplate = require('./src/menuTemplate')
-let mainWindow;
+const path = require('path')
+let mainWindow, settingsWindow
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 680,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+  const mainWindowConfig = {
+    width: 1440,
+    height: 768
+  }
   const urlLocation = isDev ? 'http://localhost:3000' : 'dummyurl'
-  mainWindow.loadURL(urlLocation)
+  mainWindow = new AppWindow(mainWindowConfig, urlLocation)
+  mainWindow.on('close', () => {
+    mainWindow = null
+  })
+  ipcMain.on('open-settings-window', () => {
+    const settingsWindowConfig = {
+      width: 500,
+      height: 400,
+      parent: mainWindow
+    }
+    const settingsFileLocation = `file://${path.join(__dirname, './settings/settings.html')}`
+    settingsWindow = new AppWindow(settingsWindowConfig, settingsFileLocation)
+    settingsWindow.on('close', () => {
+      settingsWindow = null
+    })
+  })
   const menu = Menu.buildFromTemplate(menuTemplate)
   Menu.setApplicationMenu(menu)
 })
